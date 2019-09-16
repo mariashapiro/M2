62fa9aedd016d3a2e2915a7dbbaf399dc0a52312
JohnRobinson <johnritcheyrobinson@gmail.com>
2019-09-16 10:31:05 -0400

    Person 4: fixed calc method

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -33,7 +33,7 @@ public class Person4 {
       //Person 4 put your implementation here
       String out = "";
       for (int i = 0; i < input.length(); i++) {
-        out = "" + (input.charAt(i) + 1);
+        out += "" + (input.charAt(i) + 1);
       }
       return out;
     }
